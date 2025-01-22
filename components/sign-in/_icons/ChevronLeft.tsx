c629fe895194584caf18c1015dcebc8ea80a4b66
Samyam412 <kcsamy50@gmail.com>
2025-01-22 21:45:34 +0545

    had time left over so doing some final touches

--- components/sign-in/_icons/ChevronLeft.tsx
+++ components/sign-in/_icons/ChevronLeft.tsx
@@ -8,7 +8,7 @@ function ChevronLeft() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="1"
+      strokeWidth="0.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
